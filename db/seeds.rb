df4aa853bd8425d3060cae8a2b40e1a42f4554a8
Sebastian Hahne <sebastianhahne@web.de>
2020-06-02 14:35:21 +0200

    updted posts to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,21 +13,33 @@ puts 'Cloning users'
   email = 'sebastian.remm@jamable.com'
   pw = '123456'
   username = 'Sebi'
+  mediafiles = "#{username} has no mediafiles yet"
+  caption = "Hi, my name is #{username}. I like to jam !"
   new_user = User.create!(email: email, password: pw, password_confirmation: pw, username: username)
+  new_post = Post.create!(mediafiles: mediafiles, caption: caption, user: new_user)
 
   email = 'sebastian.hahne@jamable.com'
   pw = '123456'
   username = 'Basti'
+  mediafiles = "#{username} has no mediafiles yet"
+  caption = "Hi, my name is #{username}. I like to jam !"
   new_user = User.create!(email: email, password: pw, password_confirmation: pw, username: username)
+  new_post = Post.create!(mediafiles: mediafiles, caption: caption, user: new_user)
 
   email = 'moritz.kork@jamable.com'
   pw = '123456'
   username = 'Moe'
+  mediafiles = "#{username} has no mediafiles yet"
+  caption = "Hi, my name is #{username}. I like to jam !"
   new_user = User.create!(email: email, password: pw, password_confirmation: pw, username: username)
+  new_post = Post.create!(mediafiles: mediafiles, caption: caption, user: new_user)
 
   email = 'benj.riethmuller@jamable.com'
   pw = '123456'
   username = 'Benj'
+  mediafiles = "#{username} has no mediafiles yet"
+  caption = "Hi, my name is #{username}. I like to jam !"
   new_user = User.create!(email: email, password: pw, password_confirmation: pw, username: username)
+  new_post = Post.create!(mediafiles: mediafiles, caption: caption, user: new_user)
 
 puts 'Done'
