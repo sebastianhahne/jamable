cd0a07b5411bd00da73cbfcf13a24ef6e94c8bae
Sebastian Remm <sebastian.remm@web.de>
2020-06-03 23:29:00 +0200

    whitespace in message.rb entfernt

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   include PgSearch
-  pg_search_scope :search_in_messages,  against: [:content]
+  pg_search_scope :search_in_messages, against: [:content]
 
   belongs_to :user
   belongs_to :chatroom
