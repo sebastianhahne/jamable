a1a431d9989f304da7da4269b21af7aab37c4548
Sebastian Remm <sebastian.remm@web.de>
2020-06-08 17:12:29 +0200

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_122002) do
+ActiveRecord::Schema.define(version: 2020_06_08_130438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -49,10 +49,8 @@ ActiveRecord::Schema.define(version: 2020_06_08_122002) do
     t.string "name"
     t.string "sender"
     t.string "receiver"
-    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_conversations_on_user_id"
   end
 
   create_table "follows", force: :cascade do |t|
@@ -177,7 +175,6 @@ ActiveRecord::Schema.define(version: 2020_06_08_122002) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "conversations", "users"
   add_foreign_key "friendships", "users", column: "asker_id"
   add_foreign_key "friendships", "users", column: "receiver_id"
   add_foreign_key "messages", "chatrooms"
