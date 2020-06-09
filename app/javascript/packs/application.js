6cc49547f15af2742e05e6a9a8e1d8a4e1135e32
Sebastian Remm <sebastian.remm@web.de>
2020-06-09 18:28:18 +0200

    Benj you stupid prick

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import "places";
+// import "places";
 
 // Internal imports, e.g:
 import { initChatroomCable } from '../channels/chatroom_channel';
